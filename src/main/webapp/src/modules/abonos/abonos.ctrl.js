327f22aa54bc587ed87596b2d24aedcb212b5e56
pa.alvarado10 <pa.alvarado10@ISIS2603-20.sis.virtual.uniandes.edu.co>
2016-08-29 16:14:41 -0500

    mod

---
+++ src/main/webapp/src/modules/abonos/abonos.ctrl.js
@@ -0,0 +1,111 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function (ng) {
+    var mod = ng.module("abonosModule");
+
+    mod.controller("abonosCtrl", ['$scope', '$state', '$stateParams', '$http', 'abonosContext', function ($scope, $state, $stateParams, $http, context) {
+
+            // inicialmente el listado de clientes está vacio
+            $scope.records = {};
+            // carga los clientes
+            $http.get(context).then(function(response){
+                $scope.records = response.data;    
+            }, responseError);
+
+            // el controlador recibió un clienteNombre ??
+            // revisa los parámetros (ver el :clienteNombre en la definición de la ruta)
+            if ($stateParams.abonoId !== null && $stateParams.abonoId !== undefined) {
+                
+                // toma el id del parámetro
+                id = $stateParams.abonoId;
+                // obtiene el dato del recurso REST
+                $http.get(context + "/" + id)
+                    .then(function (response) {
+                        // $http.get es una promesa
+                        // cuando llegue el dato, actualice currentRecord
+                        $scope.currentRecord = response.data;
+                    }, responseError);
+
+            // el controlador no recibió un clienteNombre
+            } else
+            {
+                // el registro actual debe estar vacio
+                $scope.currentRecord = {
+                    id: undefined /*Tipo int. El valor se asigna en el backend*/,
+            
+                };
+              
+                $scope.alerts = [];
+            }
+
+
+            this.saveRecord = function (id) {
+                currentRecord = $scope.currentRecord;
+                
+                // si el id es null, es un registro nuevo, entonces lo crea
+                if (id == null) {
+
+                    // ejecuta POST en el recurso REST
+                    return $http.post(context, currentRecord)
+                        .then(function () {
+                            // $http.post es una promesa
+                            // cuando termine bien, cambie de estado
+                            $state.go('abonosList');
+                        }, responseError);
+                        
+                // si el id no es null, es un registro existente entonces lo actualiza
+                } else {
+                    
+                    // ejecuta PUT en el recurso REST
+                    return $http.put(context + "/" + currentRecord.id, currentRecord)
+                        .then(function () {
+                            // $http.put es una promesa
+                            // cuando termine bien, cambie de estado
+                            $state.go('abonosList');
+                        }, responseError);
+                };
+            };
+
+
+
+            // -----------------------------------------------------------------
+            // Funciones para manejra los mensajes en la aplicación
+
+
+            //Alertas
+            this.closeAlert = function (index) {
+                $scope.alerts.splice(index, 1);
+            };
+
+            // Función showMessage: Recibe el mensaje en String y su tipo con el fin de almacenarlo en el array $scope.alerts.
+            function showMessage(msg, type) {
+                var types = ["info", "danger", "warning", "success"];
+                if (types.some(function (rc) {
+                    return type === rc;
+                })) {
+                    $scope.alerts.push({type: type, msg: msg});
+                }
+            }
+
+            this.showError = function (msg) {
+                showMessage(msg, "danger");
+            };
+
+            this.showSuccess = function (msg) {
+                showMessage(msg, "success");
+            };
+
+            var self = this;
+            function responseError(response) {
+
+                self.showError(response.data);
+            }
+        }]);
+
+})(window.angular);
+
+
+
---
+++ src/main/webapp/src/modules/calificaciones/calificaciones.mod.js
@@ -0,0 +1,48 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function (ng) {
+    var mod = ng.module("calificacionesModule", ["ngMessages"]);
+    mod.constant("calificacionesContext", "api/calificaciones");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/calificaciones/';
+            $urlRouterProvider.otherwise("/calificacionesList");
+     
+            $stateProvider.state('calificacionesList', {
+                url: '/calificaciones',
+                views: {
+                    'mainView': {
+                        controller: 'calificacionesCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'calificaciones.list.html'
+                    }
+                }
+            }).state('calificacionCreate', {
+                url: '/calificaciones/create',
+                views: {
+                    'mainView': {
+                        controller: 'calificacionesCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'calificaciones.create.html'
+                    }
+                }
+
+            }).state('calificacionesEdit', {
+                url: '/calificaciones/:calificacionId',
+                param: {
+                    calificacionId: null
+                },
+                views: {
+                    'mainView': {
+                        controller: 'calificacionesCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'calificaciones.create.html'
+                    }
+                }
+            });
+        }]);
+})(window.angular);
+
+

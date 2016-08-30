f4c090c9566764663d635f332bc65db9760314f1
ca.nieto11 <ca.nieto11@uniandes.edu.co>
2016-08-30 08:00:29 -0500

    Modulos iniciales de Boletas y Clientes

--- src/main/webapp/src/modules/boletas/boletas.mod.js
+++ src/main/webapp/src/modules/boletas/boletas.mod.js
@@ -1,4 +1,41 @@
-var mod = ng.module("boletasModule",["ngMessages"]);
-    
+(function (ng) {
+    var mod = ng.module("boletasModule", ["ngMessages"]);
+    mod.constant("boletasContext", "api/boletas");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/boletas/';
+            $urlRouterProvider.otherwise("/boletasList");
+     
+            $stateProvider.state('boletasList', {
+                url: '/boletas',
+                views: {
+                    'mainView': {
+                        controller: 'boletasCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'boletas.list.html'
+                    }
+                }
+            }).state('boletaCreate', {
+                url: '/boletas/create',
+                views: {
+                    'mainView': {
+                        controller: 'boletasCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'boletas.create.html'
+                    }
+                }
 
-
+            }).state('boletaEdit', {
+                url: '/boletas/:boletaId',
+                param: {
+                    boletaId: null
+                },
+                views: {
+                    'mainView': {
+                        controller: 'boletasCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'boletas.create.html'
+                    }
+                }
+            });
+        }]);
+})(window.angular);

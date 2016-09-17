ac6337600124636ef7b854904e9b5891a050f1a4
s.rodriguez20 <s.rodriguez20@ISIS2603-93.sis.virtual.uniandes.edu.co>
2016-09-16 19:55:56 -0500

    abono y calificacion eliminados del index y app

--- src/main/webapp/src/app.js
+++ src/main/webapp/src/app.js
@@ -1,6 +1,6 @@
 (function (ng) {
 
-    var mainApp = ng.module("mainApp",["ui.router","sillasModule","abonosModule","boletasModule","calificacionesModule","clientesModule","criticosModule","festivalesModule","funcionesModule","peliculasModule","salasModule","teatrosModule"]);
+    var mainApp = ng.module("mainApp",["ui.router","sillasModule","boletasModule","clientesModule","criticosModule","festivalesModule","funcionesModule","peliculasModule","salasModule","teatrosModule"]);
 
     mainApp.config(['$logProvider', function ($logProvider) {
             $logProvider.debugEnabled(true);

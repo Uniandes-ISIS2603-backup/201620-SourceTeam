ceabfa2862df1e079516c51e466bd72a4cbf0400
yabejarano10 <ya.bejarano10@uniandes.edu.co>
2016-10-06 12:06:57 -0500

    persistance

---
+++ FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/entities/SalaEntity.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.sourceteam.festivalcine.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ya.bejarano10
+ */
+public class SalaEntity extends BaseEntity implements Serializable{
+    
+}

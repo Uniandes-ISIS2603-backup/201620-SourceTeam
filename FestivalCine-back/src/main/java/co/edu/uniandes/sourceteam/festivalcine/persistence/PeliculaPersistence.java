e8594f213dd8ef2d37ff5c23c496b6d22cc4290c
yabejarano10 <ya.bejarano10@uniandes.edu.co>
2016-10-11 10:45:01 -0500

    persistence

--- FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/persistence/PeliculaPersistence.java
+++ FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/persistence/PeliculaPersistence.java
@@ -5,10 +5,58 @@
  */
 package co.edu.uniandes.sourceteam.festivalcine.persistence;
 
+import co.edu.uniandes.sourceteam.festivalcine.entities.FestivalEntity;
+import co.edu.uniandes.sourceteam.festivalcine.entities.PeliculaEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 /**
  *
  * @author ya.bejarano10
  */
-public class PeliculaPersistence {
+
+@Stateless
+public class PeliculaPersistence 
+{
+    private static final Logger LOGGER = Logger.getLogger(PeliculaPersistence.class.getName());
+
+    @PersistenceContext(unitName = "SourceteamPU")
+    protected EntityManager em;    
+ 
+    
+    public List<PeliculaEntity> findAll() {
+        LOGGER.info("Consultando todos las Películas");
+        Query q = em.createQuery("select u from PeliculaEntity u");
+        return q.getResultList();
+    }
+        
+    public PeliculaEntity create(PeliculaEntity entity) {
+        LOGGER.info("Creando una Pelicula nuevo");
+        em.persist(entity);
+        LOGGER.info("Pelicula creado");
+        return entity;
+    }
+
+    public PeliculaEntity update(PeliculaEntity entity) {
+        LOGGER.log(Level.INFO, "Actualizando Pelicula con id={0}", entity.getId());
+        return em.merge(entity);
+    }
+
+    /**
+     *
+     * @param id: corresponde a un id válido que existe el festival
+     * correspondiente en la base de datos.
+     */
+    public void delete(Long id) {
+        LOGGER.log(Level.INFO, "Borrando Pelicula con id={0}", id);
+        PeliculaEntity entity = em.find(PeliculaEntity.class, id);
+        assert entity != null;
+        em.remove(entity);
+    }
     
 }
--- FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/persistence/SalaPersistence.java
+++ FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/persistence/SalaPersistence.java
@@ -5,10 +5,58 @@
  */
 package co.edu.uniandes.sourceteam.festivalcine.persistence;
 
+import co.edu.uniandes.sourceteam.festivalcine.entities.SalaEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 /**
  *
  * @author ya.bejarano10
  */
-public class SalaPersistence {
+@Stateless
+public class SalaPersistence 
+{
+    private static final Logger LOGGER = Logger.getLogger(SalaPersistence.class.getName());
+
+    @PersistenceContext(unitName = "SourceteamPU")
+    protected EntityManager em;    
+ 
+    
+    public List<SalaEntity> findAll() {
+        LOGGER.info("Consultando todos las Salas");
+        Query q = em.createQuery("select u from SalaEntity u");
+        return q.getResultList();
+    }
+        
+    public SalaEntity create(SalaEntity entity) {
+        LOGGER.info("Creando una sala nueva");
+        em.persist(entity);
+        LOGGER.info("Sala creada");
+        return entity;
+    }
+
+    public SalaEntity update(SalaEntity entity) {
+        LOGGER.log(Level.INFO, "Actualizando Sala con id={0}", entity.getId());
+        return em.merge(entity);
+    }
+
+    /**
+     *
+     * @param id: corresponde a un id válido que existe el festival
+     * correspondiente en la base de datos.
+     */
+    public void delete(Long id) {
+        LOGGER.log(Level.INFO, "Borrando Sala con id={0}", id);
+        SalaEntity entity = em.find(SalaEntity.class, id);
+        assert entity != null;
+        em.remove(entity);
+    }
+    
+    
     
 }

f87c4de1aa556c6fc97a15424449c0786b92c7b8
yabejarano10 <ya.bejarano10@uniandes.edu.co>
2016-10-13 10:42:43 -0500

    entitys

--- FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/entities/PeliculaEntity.java
+++ FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/entities/PeliculaEntity.java
@@ -6,11 +6,67 @@
 package co.edu.uniandes.sourceteam.festivalcine.entities;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 /**
  *
  * @author ya.bejarano10
  */
-public class PeliculaEntity extends BaseEntity implements Serializable{
+@Entity
+public class PeliculaEntity extends BaseEntity implements Serializable
+{
+    @OneToOne(mappedBy = "pelicula")
+    private CriticoEntity critico = new CriticoEntity();
+    private String nombre;
+    private String anuncio;
+    private String creditos;
+    private int duracion;
+    private String pais;
+    
+    public String getNombre()
+    {
+        return  nombre;
+    }
+    public void setNombre(String n)
+    {
+        nombre = n;
+    }
+    
+    public String getAnuncio()
+    {
+        return anuncio;
+    }
+    public void setAnuncio(String a)
+    {
+        anuncio = a;
+    }
+    
+    public String getCreditos()
+    {
+        return creditos;
+    }
+    public void setCreditos(String c)
+    {
+        creditos = c;
+    }
+    
+    public int getDuracion()
+    {
+        return duracion;
+    }
+    public void setDuracion(int d)
+    {
+        duracion = d;
+    }
+    
+    public String getPais()
+    {
+        return pais;
+    }
+    public void setPais(String p)
+    {
+        pais = p;
+    }
     
 }
--- FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/entities/SalaEntity.java
+++ FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/entities/SalaEntity.java
@@ -6,11 +6,28 @@
 package co.edu.uniandes.sourceteam.festivalcine.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
 
 /**
  *
  * @author ya.bejarano10
  */
-public class SalaEntity extends BaseEntity implements Serializable{
+public class SalaEntity extends BaseEntity implements Serializable
+{
+        @OneToMany(mappedBy = "sala")
+        private TeatroEntity teatro = new TeatroEntity();
+        
+        @OneToMany(mappedBy = "sala")
+        private List<SillaEntity> sillas = new ArrayList();
+        
+        private int numSala;
+        private int numSillas;
+
+        
+        
+
     
 }

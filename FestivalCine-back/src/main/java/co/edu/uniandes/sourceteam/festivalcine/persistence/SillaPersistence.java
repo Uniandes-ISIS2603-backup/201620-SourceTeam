59ac8f4e7a6c2b1ef249305bf6f2299d916e9c79
s.rodriguez20 <s.rodriguez20@ISIS2603-93.sis.virtual.uniandes.edu.co>
2016-10-13 11:25:04 -0500

    boleta entity creada y modificaciones en cliente persistence  y silla test

--- FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/persistence/SillaPersistence.java
+++ FestivalCine-back/src/main/java/co/edu/uniandes/sourceteam/festivalcine/persistence/SillaPersistence.java
@@ -5,10 +5,59 @@
  */
 package co.edu.uniandes.sourceteam.festivalcine.persistence;
 
+import co.edu.uniandes.sourceteam.festivalcine.entities.SillaEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
 /**
  *
  * @author s.rodriguez20
  */
 public class SillaPersistence {
-    
+    private static final Logger LOGGER = Logger.getLogger(SillaPersistence.class.getName());
+
+    @PersistenceContext(unitName = "SourceteamPU")
+    protected EntityManager em;
+
+    public SillaEntity find(Long id) {
+        LOGGER.log(Level.INFO, "Consultando Silla con id={0}", id);
+        return em.find(SillaEntity.class, id);
+    }
+
+    public SillaEntity findByName(String name) {
+        LOGGER.log(Level.INFO, "Consultando Silla con name= ", name);
+        TypedQuery<SillaEntity> q
+                = em.createQuery("select u from SillaEntity u where u.name = :name", SillaEntity.class);
+        q = q.setParameter("name", name);
+        return q.getSingleResult();
+    }
+
+    public List<SillaEntity> findAll() {
+        LOGGER.info("Consultando todos los Sillas");
+        Query q = em.createQuery("select u from SillaEntity u");
+        return q.getResultList();
+    }
+
+    public SillaEntity create(SillaEntity entity) {
+        LOGGER.info("Creando un Silla nuevo");
+        em.persist(entity);
+        LOGGER.info("Silla creado");
+        return entity;
+    }
+
+    public SillaEntity update(SillaEntity entity) {
+        LOGGER.log(Level.INFO, "Actualizando Silla con id={0}", entity.getId());
+        return em.merge(entity);
+    }
+
+    public void delete(Long id) {
+        LOGGER.log(Level.INFO, "Borrando Silla con id={0}", id);
+        SillaEntity entity = em.find(SillaEntity.class, id);
+        em.remove(entity);
+    }
 }

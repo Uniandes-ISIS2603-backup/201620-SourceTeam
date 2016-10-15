2d93ee8442fc6a9a5bc3d0961cfb3f0c3b86a7e5
yabejarano10 <ya.bejarano10@uniandes.edu.co>
2016-10-15 09:42:24 -0500

    sala/peliculas persistenceTests (incompletos)

--- FestivalCine-back/src/test/java/co/edu/uniandes/sourceteam/festivalcine/testpersistance/PeliculaPersistanceTest.java
+++ FestivalCine-back/src/test/java/co/edu/uniandes/sourceteam/festivalcine/testpersistance/PeliculaPersistanceTest.java
@@ -5,10 +5,112 @@
  */
 package co.edu.uniandes.sourceteam.festivalcine.testpersistance;
 
+import co.edu.uniandes.sourceteam.festivalcine.entities.PeliculaEntity;
+import co.edu.uniandes.sourceteam.festivalcine.persistence.PeliculaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author ya.bejarano10
  */
-public class PeliculaPersistanceTest {
+public class PeliculaPersistanceTest 
+{
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(PeliculaEntity.class.getPackage())
+                .addPackage(PeliculaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    @Inject
+    private PeliculaPersistence peliculaPersistence;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    UserTransaction utx;
+
+    private List<PeliculaEntity> data = new ArrayList<PeliculaEntity>();
     
+    @Test
+    public void findAllPeliculasTest() {
+        List<PeliculaEntity> list = peliculaPersistence.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (PeliculaEntity ent : list) {
+            boolean found = false;
+            for (PeliculaEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    @Test
+    public void findPeliculaTest() {
+        PeliculaEntity entity = data.get(0);
+        PeliculaEntity newEntity = peliculaPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getName(), newEntity.getName());
+    }
+    
+    @Test
+    public void findPeliculaByNameTest() {
+        PeliculaEntity entity = data.get(0);
+        PeliculaEntity newEntity = peliculaPersistence.findByName(entity.getName());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getName(), newEntity.getName());
+    }
+    
+    @Test
+    public void createPeliculaTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        PeliculaEntity newEntity = factory.manufacturePojo(PeliculaEntity.class);
+
+        PeliculaEntity result = peliculaPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+        PeliculaEntity entity = em.find(PeliculaEntity.class, result.getId());
+        Assert.assertNotNull(entity);
+        Assert.assertEquals(newEntity.getName(), entity.getName());
+    }
+    
+    @Test
+    public void updatePeliculaTest() {
+        PeliculaEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        PeliculaEntity newEntity = factory.manufacturePojo(PeliculaEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        peliculaPersistence.update(newEntity);
+
+        PeliculaEntity resp = em.find(PeliculaEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getName(), resp.getName());
+    }
+    
+    @Test
+    public void deletePeliculaTest() {
+        PeliculaEntity entity = data.get(0);
+        peliculaPersistence.delete(entity.getId());
+        PeliculaEntity deleted = em.find(PeliculaEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
 }
--- FestivalCine-back/src/test/java/co/edu/uniandes/sourceteam/festivalcine/testpersistance/SalaPersistanceTest.java
+++ FestivalCine-back/src/test/java/co/edu/uniandes/sourceteam/festivalcine/testpersistance/SalaPersistanceTest.java
@@ -5,10 +5,115 @@
  */
 package co.edu.uniandes.sourceteam.festivalcine.testpersistance;
 
+import co.edu.uniandes.sourceteam.festivalcine.entities.PeliculaEntity;
+import co.edu.uniandes.sourceteam.festivalcine.entities.SalaEntity;
+import co.edu.uniandes.sourceteam.festivalcine.persistence.PeliculaPersistence;
+import co.edu.uniandes.sourceteam.festivalcine.persistence.SalaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author ya.bejarano10
  */
-public class SalaPersistanceTest {
+public class SalaPersistanceTest 
+{
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(SalaEntity.class.getPackage())
+                .addPackage(SalaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    @Inject
+    private SalaPersistence salaPersistence;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    UserTransaction utx;
+
+    private List<SalaEntity> data = new ArrayList<SalaEntity>();
+    
+    @Test
+    public void findAllSalasTest() {
+        List<SalaEntity> list = salaPersistence.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (SalaEntity ent : list) {
+            boolean found = false;
+            for (SalaEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    @Test
+    public void findSalaTest() {
+        SalaEntity entity = data.get(0);
+        SalaEntity newEntity = salaPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getNumSala(), newEntity.getNumSala());
+    }
+    
+    @Test
+    public void findSalaByNumeroTest() {
+        SalaEntity entity = data.get(0);
+        SalaEntity newEntity = salaPersistence.findByNumero(entity.getNumSala());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getNumSala(), newEntity.getNumSala());
+    }
+    
+    @Test
+    public void createSalaTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        SalaEntity newEntity = factory.manufacturePojo(SalaEntity.class);
+
+        SalaEntity result = salaPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+        SalaEntity entity = em.find(SalaEntity.class, result.getId());
+        Assert.assertNotNull(entity);
+        Assert.assertEquals(newEntity.getNumSala(), entity.getNumSala());
+    }
+    
+    @Test
+    public void updateSalaTest() {
+        SalaEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        SalaEntity newEntity = factory.manufacturePojo(SalaEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        salaPersistence.update(newEntity);
+
+        SalaEntity resp = em.find(SalaEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getNumSala(), resp.getNumSala());
+    }
+    
+    @Test
+    public void deleteSalaTest() {
+        SalaEntity entity = data.get(0);
+        salaPersistence.delete(entity.getId());
+        SalaEntity deleted = em.find(SalaEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
     
 }
